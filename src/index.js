ca7ba3c223d871054a0efb482ee7f23cb452065b
Julyuk <ulaukrainec@gmail.com>
2023-03-09 23:32:14 +0100

    ?

--- src/index.js
+++ src/index.js
@@ -28,6 +28,24 @@ function formatDay(timestamp) {
   return days[day];
 }
 
+function goFarenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temp");
+  celsiuslink.classList.remove("active");
+  fahrenheitlink.classList.add("active");
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+}
+function goCelsius(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
 function showPosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
@@ -45,13 +63,54 @@ function searchCity(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
+
+function getForecast(coordinates) {
+  let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+}
+
 function displayWeatherCondition(response) {
-  let tem = Math.round(response.data.main.temp);
-  document.querySelector("#temp").innerHTML = `${tem} °C`;
+  let temperatureElement = document.querySelector("#temp");
+  celsiusTemperature = response.data.main.temp;
+  temperatureElement.innerHTML = `${Math.round(celsiusTemperature)} °C`;
   document.querySelector("#city").innerHTML = response.data.name;
   let hum = Math.round(response.data.main.humidity);
   document.querySelector("#humidity").innerHTML = `${hum}%`;
-  let wind = Math.round(response.data.wind.speed);
+  let wind = Math.round(response.data.wind.speed * 3.6);
   document.querySelector("#wind-speed").innerHTML = `${wind} km/h`;
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -63,8 +122,9 @@ function displayWeatherCondition(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-
-  // getForecast(response.data.coord);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  getForecast(response.data.coord);
 }
 
 function changeCity(event) {
@@ -75,10 +135,16 @@ function changeCity(event) {
   searchCity(city);
 }
 
+let farenheitlink = document.querySelector("#f");
+farenheitlink.addEventListener("click", goFarenheit);
+
+let celsiuslink = document.querySelector("#c");
+celsiuslink.addEventListener("click", goCelsius);
+
 let buttonCurrent = document.querySelector(".button-current");
 buttonCurrent.addEventListener("click", getLocation);
 
 let searchf = document.querySelector(".button-search");
-searchf.addEventListener("submit", changeCity);
+searchf.addEventListener("click", changeCity);
 
-searchCity("London");
+searchCity("Washington");
