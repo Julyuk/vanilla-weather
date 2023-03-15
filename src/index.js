a90a6bc4c19b57c51466e9abcb0bc1753944e04e
Julyuk <ulaukrainec@gmail.com>
2023-03-15 22:36:27 +0100

    added forecast to js

--- src/index.js
+++ src/index.js
@@ -73,37 +73,23 @@ function getForecast(coordinates) {
   axios.get(apiUrl).then(displayForecast);
 }
 
-function displayForecast(response) {
-  let forecast = response.data.daily;
-  let forecastElement = document.querySelector("#forecast");
-
+function displayForecast() {
+  let forecastElement = document.querySelector("#weather-forecast");
   let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        `
-      <div class="col-2">
-        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-        <img
-          src="http://openweathermap.org/img/wn/${
-            forecastDay.weather[0].icon
-          }@2x.png"
-          alt=""
-          width="42"
-        />
-        <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${Math.round(
-            forecastDay.temp.max
-          )}° </span>
-          <span class="weather-forecast-temperature-min"> ${Math.round(
-            forecastDay.temp.min
-          )}° </span>
-        </div>
-      </div>
-  `;
-    }
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `  
+        <div class="col">
+          <p>wed</p>
+          <i class="fa-solid fa-cloud-sun weather-icon"></i>
+          <p>
+            <span class="max-temp">9° </span><span class="min-temp">8°</span>
+          </p>
+        </div>`;
   });
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function displayWeatherCondition(response) {
@@ -151,3 +137,4 @@ let searchf = document.querySelector(".button-search");
 searchf.addEventListener("click", changeCity);
 
 searchCity("Washington");
+displayForecast();
